6c8842d5a1f9204f14c4ad39f3a2cd45d03250b9
Patrick Dullano <PWDullano@gmail.com>
2016-04-20 14:54:08 -0600

    prep for heroku deployment

--- public/javascripts/services/services.js
+++ public/javascripts/services/services.js
@@ -2,32 +2,28 @@ app.service('breakService', function($http){
   var breakService = {};
 
   breakService.getMoves = function(){
-    return $http.get('http://localhost:3000/moves')
-    // return $http.get('https://breakitdown.herokuapp.com/moves')
+    // return $http.get('http://localhost:3000/moves')
+    return $http.get('https://thebreaks.herokuapp.com/moves')
   }
 
   breakService.getUser = function(){
-    return $http.get('http://localhost:3000/users')
-    // , headers: {
-      // jwt:
-      // get jwt from local storage
-    // })
-    // return $http.get('https://breakitdown.herokuapp.com/users/'+user_id, {method: "jsonp"})
+    // return $http.get('http://localhost:3000/users')
+    return $http.get('https://thebreaks.herokuapp.com/users/'+user_id, {method: "jsonp"})
   }
 
   breakService.getStripes = function(){
-    return $http.get('http://localhost:3000/stripes')
-    // return $http.get('https://breakitdown.herokuapp.com/stripes')
+    // return $http.get('http://localhost:3000/stripes')
+    return $http.get('https://thebreaks.herokuapp.com/stripes')
   }
 
   breakService.getSessions = function(){
-    return $http.get('http://localhost:3000/sessions')
-    // return $http.get('https://breakitdown.herokuapp.com/sessions')
+    // return $http.get('http://localhost:3000/sessions')
+    return $http.get('https://thebreaks.herokuapp.com/sessions')
   }
 
   breakService.newSession = function(data){
-    return $http.post('http://localhost:3000/sessions', data)
-    // return $http.post('https://breakitdown.herokuapp.com/sessions', data)
+    // return $http.post('http://localhost:3000/sessions', data)
+    return $http.post('https://thebreaks.herokuapp.com/sessions', data)
   }
 
   return breakService;
